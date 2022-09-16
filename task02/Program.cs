8aeef9ea5c0ba30dd6091e1c30c3f893b45dcb83
SvetaF <feyginas@gmail.com>
2022-09-16 20:35:08 +0300

    Добавила решение 4-й задачи

--- task02/Program.cs
+++ task02/Program.cs
@@ -2,11 +2,11 @@
 //2, 3, 7 -> 7
 Console.Clear();
 Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
-int num3 = int.Parse(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 if(num2 > max){
     max = num2;
