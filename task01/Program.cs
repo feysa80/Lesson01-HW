ac199dbc08d55546359fdc830b424c6568462d63
SvetaF <feyginas@gmail.com>
2022-09-16 19:39:34 +0300

    Исправила в задании 1 перевод строки в число

--- task01/Program.cs
+++ task01/Program.cs
@@ -2,9 +2,9 @@
 //a = 5; b = 7 -> max = 7
 Console.Clear();
 Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1 > num2){
     Console.WriteLine($"число {num1} больше чем число {num2}");
 } else if (num2 > num1){
